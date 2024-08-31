86e65eb353106a06764226c00d4f5f099153ffb4
subodh-palayekar <subodhpalayekar651@gmail.com>
2024-08-31 19:51:35 +0530

    logo changed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,6 +8,7 @@ import { logout } from '@/service/auth.service';
 import { useRouter } from 'next/navigation';
 import { useAuth } from '@/context/AuthContext';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Navbar = () => {
   const router = useRouter();
@@ -22,10 +23,11 @@ const Navbar = () => {
   return (
     <nav className="sticky top-0 z-50 w-full py-3 px-6 flex items-center justify-between border-b border-gray-300 dark:border-gray-700 bg-white/80 dark:bg-zinc-950/80 backdrop-blur-md shadow-md">
       <div className="flex items-center gap-3">
-        <Link href={'/'}>
-          <span className="text-xl font-bold tracking-wide text-zinc-900 dark:text-gray-100">
-            TelSol
-          </span>
+        <Link className="flex justify-center items-center gap-4" href={'/'}>
+          {/* <span className="text-xl font-bold tracking-wide text-zinc-900 dark:text-gray-100">
+            SB
+          </span> */}
+          <Image src="/assets/logo.png" width={40} height={40} alt="logo" />
           <span className="px-2 py-0.5 text-xs font-medium bg-gray-200 dark:bg-zinc-800 text-zinc-700 dark:text-gray-300 rounded-full">
             v1
           </span>
