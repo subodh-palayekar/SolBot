1e1beab5f403ca46573d205f5536bc5c8b3891f3
subodh-palayekar <subodhpalayekar651@gmail.com>
2024-09-16 23:51:47 +0530

    image added

--- components/Credits.tsx
+++ components/Credits.tsx
@@ -6,8 +6,12 @@ const Credits = () => {
     <div className="flex flex-wrap gap-1 fixed bottom-6">
       <p>Design & Developed By</p>
       <LinkPreview
+        height={70}
+        width={70}
         url="https://www.linkedin.com/in/subodhpalayekar/"
-        className="font-bold bg-clip-text text-transparent bg-gradient-to-br from-purple-500 to-pink-500"
+        imageSrc="/assets/subodh.jpg"
+        isStatic
+        className="font-bold bg-clip-text text-transparent  bg-gradient-to-br from-purple-500 to-pink-500"
       >
         Subodh Palayekar ❤️
       </LinkPreview>{' '}
